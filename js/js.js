9935502b110c1c3a6dda29962f78986c4339f9e5
AbubakrAliFOX <44189367+AbubakrAliFOX@users.noreply.github.com>
2018-12-06 21:57:31 +0200

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,17 +1,15 @@
-// When the user scrolls the page, execute myFunction 
-window.onscroll = function() {myFunction()};
+function displayInfo () {
+	document.querySelector('.empDetails').style.display = 'inline';
+	// document.querySelector('.p-sm2').style.fontSize = '30px'; 
+	document.querySelector('.toggleUp').style.display = 'inline';
+	document.querySelector('.toggleDown').style.display = 'none';
 
-// Get the header
-var header = document.getElementById("myHeader");
 
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+}
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
+
+function hideInfo () {
+	document.querySelector('.toggleDown').style.display = 'inline'; 
+	document.querySelector('.toggleUp').style.display = 'none';
+	document.querySelector('.empDetails').style.display = 'none';
 }
